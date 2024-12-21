2ebe2a6d59701ad3418358d9e2115369b3bd9b2e
JpNeme <jpablonj44@gmail.com>
2024-12-21 13:08:00 +0100

    Primera Subida

---
+++ kinova-phanthom_docker/Phanthom/start_omni_LAN.sh
@@ -0,0 +1,1 @@
+ufw allow 5353/udp && ./dependences/Phanthom/Touch_Setup
---
+++ kinova-phanthom_docker/Phanthom/test.sh
@@ -0,0 +1,1 @@
+chmod 777 /dev/ttyACM0 && service udev restart && udevadm control --reload-rules && udevadm trigger	&& ./dependences/Phanthom/Touch_Setup
